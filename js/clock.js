006e90ca5002ad350065a3d6e59b52b61abce121
Yujin <jyj15938@gmail.com>
2023-09-18 03:08:27 +0900

    [feat] clock

--- js/clock.js
+++ js/clock.js
@@ -1,2 +1,12 @@
 const clock = document.querySelector('h2#clock');
-clock.
+
+function getClock() {
+  const date = new Date();
+  const day = date.getDate();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  clock.innerText = `${day}일 ${hours}시 ${minutes}분`;
+}
+getClock();
+setInterval(getClock, 1000);
