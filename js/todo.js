6c33cdc0c604ccf36ffb085f4a95c0f033311599
Yujin <jyj15938@gmail.com>
2023-09-18 05:18:42 +0900

    [feat] todolist

--- js/todo.js
+++ js/todo.js
@@ -1,4 +1,58 @@
 const toDoForm = document.getElementById('todo-form');
 const toDoList = document.getElementById('todo-list');
+const toDoInput = document.querySelector('#todo-form input');
 
-function handleToDoSubmit(params) {}
+const TODOS_KEY = 'todos';
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteTodo(e) {
+  const li = e.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+
+function createList(newTodo) {
+  const li = document.createElement('li');
+  li.id = newTodo.id;
+  const span = document.createElement('span');
+  span.innerText = newTodo.text;
+  const button = document.createElement('button');
+  button.innerText = '❌';
+
+  button.addEventListener('click', deleteTodo);
+
+  li.appendChild(span);
+  li.appendChild(button);
+
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(e) {
+  e.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = '';
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  createList(newTodoObj);
+  saveToDos();
+}
+
+toDoForm.addEventListener('submit', handleToDoSubmit);
+
+const getSaveToDos = localStorage.getItem(TODOS_KEY);
+
+if (getSaveToDos !== null) {
+  const parsedToDos = JSON.parse(getSaveToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(createList);
+} else {
+}
